8356788d479d0d73430f36bc0cb8abb5bf67207c
MiNTo <markatpari@gmail.com>
2024-11-27 22:59:24 +0700

    New after login page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def authen
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get "home/index"
+
+  get 'home/authen', to: 'home#authen', as: 'home_authen'
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
