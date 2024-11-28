c372a29b1eff9ce5ef397ca5a99ccfdd4c7028b0
MiNTo <markatpari@gmail.com>
2024-11-29 05:10:32 +0700

    Updated test case

--- playtest/P01.spec.js
+++ playtest/P01.spec.js
@@ -5,10 +5,10 @@ test('test', async ({ page }) => {
   await page.goto('http://127.0.0.1:3000/home/authen');
   await page.getByRole('link', { name: 'Create a New Post' }).click();
   await page.getByLabel('Title').click();
-  await page.getByLabel('Title').fill('Newpost');
+  await page.getByLabel('Title').fill('New1');
   await page.getByLabel('Title').press('Tab');
-  await page.getByLabel('Body').fill('good dream');
+  await page.getByLabel('Body').fill('Test1');
   await page.getByRole('button', { name: 'Create Post' }).click();
   await page.getByRole('link', { name: 'Back to Home' }).click();
-  await expect(page.getByText('Detail: good dream')).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Title: New1' })).toBeVisible();
 });
--- playtest/P03.spec.js
+++ playtest/P03.spec.js
@@ -3,23 +3,23 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('http://127.0.0.1:3000/home/authen');
-  await page.getByRole('link', { name: 'Newpost' }).click();
+  await page.getByRole('link', { name: 'Create a New Post' }).click();
+  await page.getByLabel('Title').click();
+  await page.getByLabel('Title').fill('New1');
+  await page.getByLabel('Title').press('Tab');
+  await page.getByLabel('Body').fill('Test1');
+  await page.getByRole('button', { name: 'Create Post' }).click();
+  await page.getByRole('link', { name: 'Back to Home' }).click();
+  await expect(page.getByRole('heading', { name: 'Title: New1' })).toBeVisible();
+  await page.getByRole('link', { name: 'New1' }).click();
   await page.getByRole('link', { name: 'Edit this post' }).click();
   await page.getByLabel('Body').click();
-  await page.getByLabel('Body').fill('good bye');
+  await page.getByLabel('Body').fill('Test2');
   await page.getByRole('button', { name: 'Update Post' }).click();
   await page.getByRole('link', { name: 'Back to Home' }).click();
-  await expect(page.getByText('Detail: good bye')).toBeVisible();
-  await page.getByRole('link', { name: 'Newpost' }).click();
+  await expect(page.getByText('Detail: Test2 Comments:')).toBeVisible();
+  await page.getByRole('link', { name: 'New1' }).click();
   await page.getByRole('button', { name: 'Destroy this post' }).click();
   await page.getByRole('link', { name: 'Back to Home' }).click();
-  await expect(page.locator('body')).toMatchAriaSnapshot(`
-    - paragraph
-    - paragraph
-    - heading "Welcome to the Authenticated Page!" [level=1]
-    - heading "Posts" [level=1]
-    - link "Create a New Post"
-    - link "Back to Home"
-    - link "Log out"
-    `);
+  await expect(page.getByText('Welcome to the Authenticated Page! Posts Create a New Post Back to Home Log out')).toBeVisible();
 });
