16fb889fab253aae64dc310e4942d78fa1da1ac1
MiNTo <markatpari@gmail.com>
2024-11-28 08:59:42 +0700

    Post link

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,13 @@ class PostsController < ApplicationController
 
   # GET /posts/1 or /posts/1.json
   def show
+    respond_to do |format|
+      format.turbo_stream
+      format.html
+    end
   end
+  
+  
 
   # GET /posts/new
   def new
@@ -22,47 +28,55 @@ class PostsController < ApplicationController
   # POST /posts or /posts.json
   def create
     @post = Post.new(post_params)
-  
     respond_to do |format|
       if @post.save
         format.turbo_stream do
           render turbo_stream: turbo_stream.append("posts", partial: "posts/post", locals: { post: @post })
         end
         format.html { redirect_to @post, notice: "Post was successfully created." }
-        format.json { render :show, status: :created, location: @post }
       else
         format.turbo_stream do
           render turbo_stream: turbo_stream.replace("new_post_form", partial: "posts/form", locals: { post: @post })
         end
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
       end
     end
   end
   
+  
 
   # PATCH/PUT /posts/1 or /posts/1.json
   def update
-    respond_to do |format|
-      if @post.update(post_params)
+    if @post.update(post_params)
+      respond_to do |format|
+        format.turbo_stream { render turbo_stream: turbo_stream.replace(@post, partial: "posts/post", locals: { post: @post }) }
         format.html { redirect_to @post, notice: "Post was successfully updated." }
         format.json { render :show, status: :ok, location: @post }
-      else
+      end
+    else
+      respond_to do |format|
+        format.turbo_stream { render turbo_stream: turbo_stream.replace(@post, partial: "posts/form", locals: { post: @post }) }
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @post.errors, status: :unprocessable_entity }
       end
     end
   end
+  
+  
+  
 
   # DELETE /posts/1 or /posts/1.json
   def destroy
     @post.destroy!
-
     respond_to do |format|
+      format.turbo_stream { render turbo_stream: turbo_stream.remove(@post) }
       format.html { redirect_to posts_path, status: :see_other, notice: "Post was successfully destroyed." }
       format.json { head :no_content }
     end
   end
+  
+  
+  
 
   private
     # Use callbacks to share common setup or constraints between actions.
